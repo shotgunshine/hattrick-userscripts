a9849b5938e51a61778cb291f0407475bbff7808
shotgunshine <mc517053@gmail.com>
2024-04-16 22:07:44 +0200

    add match prediction script

--- Hattrick custom shortcuts.user.js
+++ Hattrick custom shortcuts.user.js
@@ -15,6 +15,7 @@
         {url: '/Club/Transfers/', icon: 'scBids', alt: 'Aste in corso'},
         {url: '/Club/Matches/MatchOrder/Matchorder.aspx?MatchID=0&SourceSystem=Hattrick&teamId=0', icon: 'scMatches2', alt: 'Simulatore di formazione'},
         {url: '/Club/TacticsRoom/', icon: 'scAnalyzer', alt: 'Ufficio del tattico'},
+        {url: 'https://shotgunshine.github.io/imp/', icon: 'f_match', alt: 'IMP: Match Predictor'},
     ];
 
     let s = document.querySelector('.scContainerNoSupporter');
---
+++ Hattrick match prediction.user.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name         Hattrick match prediction
+// @version      2024-04-15
+// @description  
+// @author       shotgunshine
+// @license      MIT
+// @match        https://*.hattrick.org/Club/Matches/Match.aspx*
+// @match        https://*.hattrick.org/*/Club/Matches/Match.aspx*
+// @grant        none
+// @require      https://shotgunshine.github.io/imp/imp/binomial.js
+// @require      https://shotgunshine.github.io/imp/imp/ratings.js
+// @require      https://shotgunshine.github.io/imp/imp/predictor.js
+// ==/UserScript==
+
+function getRatings(ratings, tacticType, tacticSkill) {
+    return new IMP.ratings(
+        ratings.averageLeftDef,
+        ratings.averageMidDef,
+        ratings.averageRightDef,
+        ratings.averageMidfield,
+        ratings.averageLeftAtt,
+        ratings.averageMidAtt,
+        ratings.averageRightAtt,
+        ratings.averageIndirectFreeKickDef,
+        ratings.averageIndirectFreeKickAtt,
+        0.25,
+        tacticType,
+        tacticSkill
+    );
+}
+
+function getOdds(prediction) {
+    let odds = [0, 0, 0];
+    prediction.forEach((x, h) => { x.forEach((y, a) => { odds[Math.sign(a - h) + 1] += y })});
+    return {
+        win: (odds[0] * 100).toFixed(1),
+        draw: (odds[1] * 100).toFixed(1),
+        loss: (odds[2] * 100).toFixed(1)
+    };
+}
+
+(function() {
+    'use strict';
+
+    let match = window.HT.ngMatch.data;
+
+    if (match.isFinished && !match.isWalkover && match.sourceSystem != 'Youth') {
+        let home = getRatings(match.ratings.filter(x => x.teamId ==  match.homeTeamIdDB)[0], match.homeTacticType, match.homeTacticSkill);
+        let away = getRatings(match.ratings.filter(x => x.teamId ==  match.awayTeamIdDB)[0], match.awayTacticType, match.awayTacticSkill);
+        let possession = IMP.predictor.chanceDistribution(home.midfield, away.midfield);
+        let pressing = IMP.predictor.tacticEfficacy(1, home.tactics[1]);
+        pressing += IMP.predictor.tacticEfficacy(1, away.tactics[1]);
+        let avgScoringHome = IMP.predictor.avgScoringChance(home, away);
+        let avgScoringAway = IMP.predictor.avgScoringChance(away, home);
+        let caThreshold = home.tactics[2]*away.tactics[2] ? 0.5 : 0.43245;
+        let countersHome = (possession < caThreshold) ? IMP.predictor.tacticEfficacy(2, home.tactics[2]) : 0;
+        let countersAway = (1 - possession < caThreshold) ? IMP.predictor.tacticEfficacy(2, away.tactics[2]) : 0;
+        let avgChancesHome = possession + countersHome * (1 - possession) * (1 - avgScoringAway);
+        let avgChancesAway = (1 - possession) + countersAway * possession * (1 - avgScoringHome);
+
+        let prediction = getOdds(IMP.predictor.prediction(
+            possession,
+            pressing,
+            avgScoringHome,
+            avgScoringAway,
+            countersHome,
+            countersAway
+        ));
+
+        document.querySelector('.matchinfo > .boxBody').innerHTML += `
+            <div><hr><p class="center"><a href="https://shotgunshine.github.io/imp/" target="_blank">IMP: Match Predictor</a></p>
+            <div class="flex flex-space-between">
+            <div>${window.HT.ngMatch.data.homeShortTeamName}</div>
+            <div>Draw</div>
+            <div>${window.HT.ngMatch.data.awayShortTeamName}</div></div>
+            <div style="margin: 5px 0; line-height: 1; display: flex;">
+            <div style="padding: 2px 5px; background-color: #6ecdea; text-align: left; width: ${prediction.win}%;"><span>${prediction.win}%</span></div>
+            <div style="padding: 2px 5px; background-color: #eeeeee; text-align: center; width: ${prediction.draw}%;"><span>${prediction.draw}%</span></div>
+            <div style="padding: 2px 5px; background-color: #d15e5e; text-align: right; width: ${prediction.loss}%;"><span>${prediction.loss}%</span></div>
+            </div><div class="flex flex-space-between">
+            <div>${(10 * avgChancesHome * (1 - pressing) * avgScoringHome).toFixed(2)}</div>
+            <div>Average Goals</div>
+            <div>${(10 * avgChancesAway * (1 - pressing) * avgScoringAway).toFixed(2)}</div>
+            </div></div>
+        `;
+    }
+})();
